4d4cbe7db10e97e8b385ac6caaf4bdada5056fee
Amélie Darouechi <ameliedarouechi@MacBook-Pro-de-Amelie.local>
2025-09-03 04:15:26 +0200

    Change css design and bandeau rouge

--- src/components/Etape.tsx
+++ src/components/Etape.tsx
@@ -22,7 +22,7 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
 
   return (
     <main className="w-full h-[100dvh] flex flex-col items-center justify-center overflow-hidden">
-      <div className="relative w-[520px] h-full">
+      <div className="relative w-full h-full">
         {/* Fond */}
         <Image
           src={noeud.image}
@@ -42,7 +42,7 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
               <div className="absolute inset-0 flex items-center justify-center mb-10">
                 <Link
                   href={`/etape/${choicesText[0].suivant}`}
-                  className="bg-red-700 text-white text-base sm:text-lg font-bold text-center py-3 p-7 hover:bg-red-800 transition w-full max-w-[520px]"
+                  className="bg-red-700 text-white text-base sm:text-lg font-bold text-center  hover:bg-red-800 transition py-4 px-6 w-full sm:max-w-[475px]"
                 >
                   {choicesText[0].libelle}
                 </Link>
@@ -54,7 +54,7 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
               <div className="absolute inset-x-0 bottom-0 flex justify-center">
                 <Link
                   href={`/etape/${choicesText[1].suivant}`}
-                  className="bg-red-700 text-white text-base sm:text-lg font-bold text-center py-3 p-7 hover:bg-red-800 transition w-full max-w-[520px]"
+                  className="bg-red-700 text-white text-base sm:text-lg font-bold text-center py-4 px-6 hover:bg-red-800 transition w-full sm:max-w-[475px]"
                 >
                   {choicesText[1].libelle}
                 </Link>
@@ -64,47 +64,10 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
         )}
 
         {/* ===================== CAS SANS CHOIX (CSS initial gardé) ===================== */}
-        {/* {!hasTextChoices && !hasVisualChoices && (
-          <div className="absolute inset-x-0 flex flex-col items-center justify-end bottom-0 text-center">
-            {isIntro ? (
-              <div className="mb-4 bg-red-700 backdrop-blur-sm px-6 py-5 rounded-xl shadow-lg max-w-md w-[85%]">
-                {noeud.titre && (
-                  <h1 className="text-3xl sm:text-4xl font-extrabold text-white uppercase">
-                    {noeud.titre}
-                  </h1>
-                )}
-                {noeud.sousTitre && (
-                  <p className="mt-2 text-lg sm:text-2xl font-bold text-white uppercase">
-                    {noeud.sousTitre}
-                  </p>
-                )}
-              </div>
-            ) : isEnonce ? (
-              <div className="bg-red-700 px-6 py-4 pb-7 shadow-lg max-w-md w-[80%] mx-auto text-center">
-                <p className="text-white mb-5 text-base sm:text-lg font-semibold">
-                  {noeud.texte}
-                </p>
-                <Link
-                  href={`/etape/${noeud.suivant}`}
-                  className="px-4 py-2 bg-white text-red-700 font-semibold rounded-lg shadow-md hover:bg-gray-100 transition"
-                >
-                  {noeud.libelleSuivant ?? "Suivant"}
-                </Link>
-              </div>
-            ) : (
-              noeud.texte && (
-                <div className="bg-red-700/90 px-6 py-4 rounded-lg shadow-lg max-w-md w-[80%] mx-auto text-center">
-                  <p className="text-white text-lg sm:text-2xl font-bold drop-shadow md:px-4">
-                    {noeud.texte}
-                  </p>
-                </div>
-              )
-            )} */}
-
             {!hasTextChoices && !hasVisualChoices && (
             <div className="absolute inset-x-0 bottom-0 flex flex-col items-center justify-end text-center mb-4">
               {isIntro ? (
-                <div className="bg-red-700 px-6 py-5 shadow-lg w-full">
+                <div className="bg-red-700 py-4 px-6 w-full sm:max-w-[475px]">
                   {noeud.titre && (
                     <h1 className="text-3xl sm:text-4xl font-extrabold text-white uppercase">
                       {noeud.titre}
@@ -117,7 +80,7 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
                   )}
                 </div>
               ) : isEnonce ? (
-                <div className="bg-red-700 px-6 py-6 shadow-lg w-full">
+                <div className="bg-red-700 py-4 px-6 w-full sm:max-w-[475px]">
                   <p className="text-white mb-5 text-base sm:text-lg font-semibold">
                     {noeud.texte}
                   </p>
@@ -130,7 +93,7 @@ export default function Etape({ noeud }: { noeud: Noeud }) {
                 </div>
               ) : (
                 noeud.texte && (
-                  <div className="bg-red-700 px-6 py-6 shadow-lg w-full">
+                  <div className="bg-red-700 py-4 px-6 w-full sm:max-w-[475px]">
                     <p className="text-white text-lg sm:text-2xl font-bold drop-shadow">
                       {noeud.texte}
                     </p>
